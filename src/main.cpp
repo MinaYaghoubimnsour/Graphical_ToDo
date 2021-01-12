48f658348fb78644c10e306619908b033387e3d3
fateme-nabati <fatemenabati22@gmail.com>
2021-01-12 09:57:06 -0800

    change background and set calendar icon

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,21 @@ int main()
 {
   //object of class RenderWindow and Event
 
-  sf::RenderWindow window(sf::VideoMode(605, 800), "To Do");
+  sf::RenderWindow window(sf::VideoMode(1402, 789), "To Do");
   sf::Event event;
 
   // background
   sf::Texture backgroundTexture;
   sf::Sprite backgroundSpr;
-  backgroundTexture.loadFromFile("/home/fn/exercise5_991/background.jpg");
+  backgroundTexture.loadFromFile("/home/fn/exercise5_991/background1.jpg");
   backgroundSpr.setTexture(backgroundTexture);
+
+  //icon calendar
+  sf::Texture calendarTexture;
+  sf::Sprite calendarSpr;
+  calendarTexture.loadFromFile("/home/fn/exercise5_991/schedule.png");
+  calendarSpr.setTexture(calendarTexture);
+  calendarSpr.setPosition(sf::Vector2f(10, 0));
   while (window.isOpen())
   {
     while (window.pollEvent(event))
@@ -24,6 +31,7 @@ int main()
       }
     }
     window.draw(backgroundSpr); // draw picture of background
+    window.draw(calendarSpr);   // draw calendar icon
     window.display(); // display window
   }
   return 0;
